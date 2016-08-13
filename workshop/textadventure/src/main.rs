eead67bde0d4ad3dff796d832693e071d5f95021
Frank Vasquez <Frank.Vasquez@gmail.com>
2016-08-13 14:30:22 -0700

    take turns
    Fill in main with turn-based gameplay using a real player and 2 NPCs.

--- workshop/textadventure/src/main.rs
+++ workshop/textadventure/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
+struct Player {}
+
+impl Player {
+    fn take_turn(&self) -> bool {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        // TODO parse and process input
+
+        return true;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Gnomes! Let's begin.");
+
+    let player = Player {};  // user
+    let gnome = Player {};  // NPC
+    let leprechaun = Player {};  // NPC
+
+    loop {
+        if !player.take_turn() {  // not alive
+            break;
+        }
+
+        gnome.take_turn();
+        leprechaun.take_turn();
+    }
+
+    println!("You have died!  GAME OVER");
 }
