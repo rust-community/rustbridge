4064d322865fc35d8873bc967e4965dedfaff914
crhino <piraino.chris@gmail.com>
2016-08-13 14:17:39 -0700

    Add db module to insert and retrieve Link objects

--- workshop/url_shortener/src/main.rs
+++ workshop/url_shortener/src/main.rs
@@ -7,35 +7,14 @@ extern crate nickel;
 
 mod serdes;
 mod server;
+mod db;
 
-use rustc_serialize::base64::ToBase64;
-use rustc_serialize::base64::Newline::*;
-use rustc_serialize::base64::CharacterSet::*;
-use rustc_serialize::base64::Config;
+use db::{LinksDB};
 
 use nickel::{Nickel, HttpRouter};
 use rusqlite::SqliteConnection;
 use time::Timespec;
 use server::start_server;
-
-//
-// DATA MODELS
-//
-#[derive(Debug)]
-struct Link {
-    id: u64,
-    target: String,
-    use_count: u64,
-    created_at: Timespec
-}
-
-#[derive(Debug)]
-struct LinkUse {
-    id: u64,
-    link_id: u64,
-    browser_info: String,
-    created_at: Timespec
-}
 
 //
 // WEB SERVER
@@ -46,5 +25,6 @@ fn main() {
         serdes::decode(serdes::encode(i));
     }
 
+    let links_db = LinksDB::with_tables().unwrap();
     start_server();
 }
