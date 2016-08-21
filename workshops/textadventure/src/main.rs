8a519c5cdf6e53143f940414c8a395232c41de19
Frank Vasquez <Frank.Vasquez@gmail.com>
2016-08-20 21:52:22 -0700

    replace traits with an enum
    transforms instead of mutable references

--- workshops/textadventure/src/main.rs
+++ workshops/textadventure/src/main.rs
@@ -3,20 +3,16 @@ mod players;
 
 use board::build_board;
 use players::build_players;
+use players::take_turn;
 
 fn main() {
     println!("Welcome to the maze! Let's begin.");
 
     let board = build_board();
-    let players = build_players();
+    let mut players = build_players();
 
     loop {
-        for player in &players {
-            if !player.take_turn(&board, &players) {
-                println!("You have died!");
-                break;
-            }
-        }
+        take_turn(&mut players, &board);
     }
 
     println!("GAME OVER");
--- workshops/textadventure/src/players.rs
+++ workshops/textadventure/src/players.rs
@@ -1,95 +1,72 @@
 use board::Board;
+use std::collections::VecDeque;
 use std::io;
 
-pub trait Player {
-    fn take_turn(&self, board: &Board, players: &Players) -> bool;
+struct ExplorerData { x: u32, y: u32, energy: i32 }
+struct GnomeData { x: u32, y: u32 }
+struct LeprechaunData { x: u32, y: u32 }
+
+enum Player {
+    Explorer(ExplorerData),  // actual player
+    Gnome(GnomeData),  // NPC
+    Leprechaun(LeprechaunData),  // NPC
 }
 
-pub type Players = Vec<Box<Player>>;
-
-trait Movable {
-    fn move_up(&self, &Board) -> bool;
-    fn move_right(&self, &Board) -> bool;
-    fn move_down(&self, &Board) -> bool;
-    fn move_left(&self, &Board) -> bool;
-}
-
-trait Teleportable {
-    fn teleport(&self, &Board) {
-        // TODO Jump to any other space on the game board at random.
-        unimplemented!(); 
-    }
-}
-
-pub struct Explorer { x: u32, y: u32, energy: i32 }  // actual player
-struct Gnome { x: u32, y: u32 }  // NPC
-struct Leprechaun { x: u32, y: u32 }  // NPC
-
-impl Player for Explorer {
-    fn take_turn(&self, board: &Board, players: &Players) -> bool {
-        let mut input = String::new();
-        
-        io::stdin().read_line(&mut input)
-                .expect("Failed to read line");
-
-        // TODO Parse input and move or teleport Explorer accordingly.
-        // If Explorer has a Teleporter and chooses to use it then teleport.
-        // Otherwise attempt to move Explorer one space left, right, up or down.
-        unimplemented!();
-
-        true  // TODO Return false when Explorer runs out of energy.
-     }
-}
-
-impl Movable for Explorer {
-    // TODO Attempt to Move Explorer one space away from current x, y position.
-    // A move will fail if an Explorer attempts to pass through a Wall or 
-    // travel outside the boundaries of the Board.
-    fn move_up(&self, board: &Board) -> bool {unimplemented!(); false }
-    fn move_right(&self, board: &Board) -> bool { unimplemented!(); false }
-    fn move_down(&self, board: &Board) -> bool { unimplemented!(); false }
-    fn move_left(&self, board: &Board) -> bool { unimplemented!(); false }
-}
-
-impl Teleportable for Explorer {}
-
-impl Player for Gnome {
-    fn take_turn(&self, board: &Board, players: &Players) -> bool {
-        // TODO Move Gnome one space left, right, up or down.
-        // Do not waste a turn attempting to move outside the Board.
-        unimplemented!();
-
-        true  // Gnomes do not die.
-    }
-}
-
-impl Movable for Gnome {
-    // TODO Move Gnome one space away from current x, y position.
-    // Gnomes can pass through any Wall: Opening, Magical or Solid.
-    // Gnomes cannot leave the confines of the board.
-    fn move_up(&self, board: &Board) -> bool { unimplemented!(); false }
-    fn move_right(&self, board: &Board) -> bool { unimplemented!(); false }
-    fn move_down(&self, board: &Board) -> bool { unimplemented!(); false }
-    fn move_left(&self, board: &Board) -> bool { unimplemented!(); false }
-}
-
-impl Player for Leprechaun {
-    fn take_turn(&self, board: &Board, players: &Players) -> bool {
-        // TODO Teleport Leprechaun to any space on the Board at random.
-        unimplemented!();
-
-        true  // Leprechauns do not die.
-    }
-}
-
-impl Teleportable for Leprechaun {}
+pub type Players = VecDeque<Player>;
 
 pub fn build_players() -> Players {
-    let mut players: Players = Vec::new();
+    let mut players: Players = VecDeque::new();
 
-    players.push(Box::new(Explorer { x: 0, y: 0, energy: 99 }));
-    players.push(Box::new(Gnome { x: 0, y: 4 }));
-    players.push(Box::new(Leprechaun { x: 4, y: 4 }));
+    players.push_back(Player::Explorer(ExplorerData { x: 0, y: 0, energy: 99 }));
+    players.push_back(Player::Gnome(GnomeData { x: 0, y: 4 }));
+    players.push_back(Player::Leprechaun(LeprechaunData { x: 4, y: 4 }));
 
     players
+}
+
+pub fn take_turn(players: &mut Players, board: &Board) {
+    let _player: Player;
+
+    match players.pop_front() {
+        Some(player) => match player {
+            Player::Explorer(data) => {
+                let _data = play_explorer(data, players, board);
+                _player = Player::Explorer(_data);
+            },
+            Player::Gnome(data) => {
+                let _data = play_gnome(data, players, board);
+                _player = Player::Gnome(_data);
+            },
+            Player::Leprechaun(data) => {
+                let _data = play_leprechaun(data, players, board);
+                _player = Player::Leprechaun(_data);
+            },
+        },
+        None => return,
+    }
+
+    players.push_back(_player)
+}
+
+fn play_explorer(data: ExplorerData, others: &Players, board: &Board) -> ExplorerData {
+    ExplorerData { x: data.x, y: data.y, energy: data.energy }
+}
+
+fn play_gnome(data: GnomeData, others: &Players, board: &Board) -> GnomeData {
+    GnomeData { x: data.x, y: data.y }
+}
+
+fn play_leprechaun(data: LeprechaunData, others: &Players, board: &Board) -> LeprechaunData {
+    let mut _data = data;
+    teleport_leprechaun(&mut _data, board);
+    _data
+}
+
+fn teleport_explorer(data: &mut ExplorerData, board: &Board) -> bool {
+    false
+}
+
+fn teleport_leprechaun(data: &mut LeprechaunData, board: &Board) {
+    data.x = 4;
+    data.y = 0;
 }
