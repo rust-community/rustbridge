906b7375090873530cc44b5efc394edc152adb2f
Martin Broesamle <m@m-broe.de>
2016-08-26 00:07:24 +0200

    Change the format of the queue to tuples (x,y).

--- workshops/mondrian-pattern/codebase/multipaint/src/main.rs
+++ workshops/mondrian-pattern/codebase/multipaint/src/main.rs
@@ -1,4 +1,4 @@
-// Based on the final version of the multiprint client/server/shared queue example. 
+// Based on the final version of the multiprint client/server/shared queue example.
 // https://github.com/broesamle/RustWorkshop/blob/master/minimals/multiprint.md
 
 use std::thread;
@@ -7,17 +7,17 @@ use std::time::Duration;
 
 fn main() {
     let mut threads = Vec::new();
-    let printqueue: Vec<String> = Vec::new();
-    let printqueue_mutex_arc = Arc::new(Mutex::new(printqueue));
+    let queue: Vec<(u64, u64)> = Vec::new();
+    let printqueue_mutex_arc = Arc::new(Mutex::new(queue));
     let serverqueue = printqueue_mutex_arc.clone();
     let server = thread::spawn(move || {
         loop {
             if let Ok(mut guard) = serverqueue.try_lock() {
-                if let Some(printjob) = (*guard).pop() {
-                    println!("printing: {}", printjob);
+                if let Some((x, y)) = (*guard).pop() {
+                    println!("position {}, {}", x, y);
                 }
             }
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_millis(10));
         }
     });
     for num in 0..10 {
@@ -26,9 +26,8 @@ fn main() {
             let mut i = 0;
             loop {
                 if let Ok(mut guard) = clientqueue.try_lock() {
-                    let job = format!("Job {} from Child {}.", i, num);
-                    println!("putting: {}", job);
-                    (*guard).push(job);
+                    println!("putting: {:?}", (i, num));
+                    (*guard).push((i, num));
                     i += 1;
                 }
                 thread::sleep(Duration::from_millis(500*(num+1)));
