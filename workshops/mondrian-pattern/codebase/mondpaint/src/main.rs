e832204ebebd2ea4efb3b4970cc5a64a159459eb
Martin Broesamle <m@minusbroe.de>
2016-09-04 22:48:20 +0200

    Fail in a funny way (infinite recursion).

--- workshops/mondrian-pattern/codebase/mondpaint/src/main.rs
+++ workshops/mondrian-pattern/codebase/mondpaint/src/main.rs
@@ -66,7 +66,7 @@ fn vsplit_and_paint(x :f64, y :f64, width :f64, height :f64, chn: SendChannel) {
     thread::sleep(Duration::from_millis(500));
     let chnright = chn.clone();
     let rightpainterthread = thread::spawn(move ||
-        paint_rectangle(x+splitpos, y, width-splitpos, height, BLUE, chnright)
+        hsplit_and_paint(x+splitpos, y, width-splitpos, height, chnright)
     );
     leftpainterthread.join().unwrap();
     rightpainterthread.join().unwrap();
