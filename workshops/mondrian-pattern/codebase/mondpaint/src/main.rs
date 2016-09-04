86293c4533b2b9b3847159cf6893de55ac860077
Martin Broesamle <m@minusbroe.de>
2016-09-04 20:29:30 +0200

    Adapt naming; predifine what learners will need.
    Comment out randomisation stuff.
    Streamline operations names and arguments.
    Remove unnecessary console output and comments.
    Set window title.
    Add more colours.

--- workshops/mondrian-pattern/codebase/mondpaint/src/main.rs
+++ workshops/mondrian-pattern/codebase/mondpaint/src/main.rs
@@ -1,5 +1,3 @@
-// Combines the multiprint and graphout https://github.com/broesamle/RustWorkshop/blob/master/minimals/multiprint.md examples into a minimal multithreaded 'paint server' scheme.
-
 extern crate piston;
 extern crate glutin_window;
 extern crate opengl_graphics;
@@ -14,7 +12,7 @@ use piston::input::RenderEvent;
 use graphics::clear;
 use graphics::types::{Rectangle, Color};
 
-use rand::Rng;
+//use rand::Rng;
 
 use std::thread;
 use std::sync::mpsc;
@@ -22,16 +20,19 @@ use std::time::Duration;
 
 const WHITE:   [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 const RED:     [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const GREEN:   [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+const BLUE:    [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+const YELLOW:  [f32; 4] = [1.0, 1.0, 0.0, 1.0];
 const BLACK:   [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
 fn main() {
     let (paintsend, paintrecv) = mpsc::channel();
 
-    let serverthread = thread::spawn(move || {
+    let gfxserverthread = thread::spawn(move || {
         let mut pleaseinit = true;
         // prepare graphics output + window management
         let mut window: Window =
-            WindowSettings::new("Hello World!", [512; 2])
+            WindowSettings::new("RustBridge / Mondrian Pattern Generator", [512; 2])
                 .build().unwrap();
         let opengl = OpenGL::V3_2;
         let mut gl = GlGraphics::new(opengl);
@@ -46,8 +47,8 @@ fn main() {
                         clear(WHITE, gl);
                         pleaseinit = false;
                     }
-                    if let Ok( (rct, col) ) = paintrecv.recv() {
-                        println!("Received: {:?}", (rct, col) );
+                    if let Ok( (rct, col) ) = paintrecv.try_recv() {
+                        println!("GFX-OUTPUT: rect {:?}", (rct, col) );
                         redrect.color(col).draw(rct, &c.draw_state, c.transform, gl);
                     }
                 });
@@ -55,39 +56,34 @@ fn main() {
         }
     });
     let chn = paintsend.clone();
-    let clientthread = thread::spawn(move ||
-        delegatemondrian([20.0, 20.0, 300.0, 250.0], chn)
+    let patternpainterthread = thread::spawn(move ||
+        paint_rectangle(20.0, 20.0, 300.0, 250.0, chn)
     );
-    println!("Started master painter.");
 
-    let _ = clientthread.join();
-    let _ = serverthread.join();
+    patternpainterthread.join().unwrap();
+    gfxserverthread.join().unwrap();
 }
 
-fn delegatemondrian(r: Rectangle, chn: mpsc::Sender<(Rectangle, Color)>) {
-    let mut rng = rand::thread_rng();   //init a random number generator
+fn paint_rectangle(x :f64, y :f64, width :f64, height :f64, chn: mpsc::Sender<(Rectangle, Color)>)
+{
+    println! ( "paint_rectangle: {:}, {:}, {:}, {:}", x, y, width, height);
+    chn.send( ([x, y, width, height], RED) ).unwrap();
+}
 
-    let (x, y, w, h) = (r[0], r[1], r[2], r[3]);
-    let splitpos = rng.gen_range(0.0, w);
-    let leftsection: Rectangle = [x, y, splitpos, h];
-    let rightsection: Rectangle = [x+splitpos, y, w-splitpos, h];
+fn vsplit_and_paint(x :f64, y :f64, width :f64, height :f64, chn: mpsc::Sender<(Rectangle, Color)>) {
+    // let mut rng = rand::thread_rng();   //init a random number generator
+
+    let splitpos = width / 2.0;
 
     let chnleft = chn.clone();
-    let leftpainter = thread::spawn(move ||
-        elementarymondrian(leftsection, chnleft)
+    let leftpainterthread = thread::spawn(move ||
+        paint_rectangle(x, y, splitpos, height, chnleft)
     );
     thread::sleep(Duration::from_millis(500));
-    println!("Delegated left.");
     let chnright = chn.clone();
-    let rightpainter = thread::spawn(move ||
-        elementarymondrian(rightsection, chnright)
+    let rightpainterthread = thread::spawn(move ||
+        paint_rectangle(x+splitpos, y, width-splitpos, height, chnright)
     );
-    println!("Delegated right.");
-    let _ = leftpainter.join();
-    let _ = rightpainter.join();
-}
-
-fn elementarymondrian(r: Rectangle, chn: mpsc::Sender<(Rectangle, Color)>) {
-    println! ( "putting: {:?}", (r, RED) );
-    chn.send( (r, RED) ).unwrap();
+    let _ = leftpainterthread.join();
+    let _ = rightpainterthread.join();
 }
